3cba63d5186cc187c8b12944edc87114a52f5c62
Santiago Barreda <sbarreda@ucdavis.edu>
2024-02-26 13:44:17 -0800

    Update loadsound.R

--- R/loadsound.R
+++ R/loadsound.R
@@ -1,10 +1,3 @@
-# Copyright (c) 2015 Santiago Barreda
-# All rights reserved.
-
-
-
-
-
 
 #' Load WAV files into R
 #' 
@@ -14,16 +7,20 @@
 #' function returns a 'sound object'. Many of the functions included in this
 #' package interact with 'sound' objects.
 #' 
+#' @export
 #' @aliases loadsound print.sound plot.sound
 #' @param filename A string indicating the file name of the WAV file to be
 #' loaded. If no filename is provided, a dialog box will open allowing the user
 #' to select a file.
+#' @param channel A string indicating the channel to be loaded. The default is
+#' 'left'. If 'right' is selected, the right channel will be loaded.
 #' @return An object of class 'sound', a list containing the elements:
 #' 
 #' \item{filename}{A vector containing the filename of the WAV file.}
-#' \item{fs}{The sampling frequency of the sound. } \item{duration}{The
-#' duration of the sound, in milliseconds. } \item{sound}{A vector of numeric
-#' values representing the sampled sound. }
+#' \item{fs}{The sampling frequency of the sound. } 
+#' \item{duration}{The duration of the sound, in milliseconds. } 
+#' \item{sound}{A vector of numeric values representing the sampled sound. }
+#' \item{bit}{The wav file bit rate. }
 #' @author Santiago Barreda <sbarreda@@ucdavis.edu>
 #' @references https://ccrma.stanford.edu/courses/422/projects/WaveFormat/
 #' @examples
@@ -35,44 +32,21 @@
 #' ## plot (sound)
 #' ## spectrogram (sound)
 #' 
-loadsound = function (filename=0){
-  if (mode(filename)!="character") filename = file.choose()
-
-  soundfile = file(filename,"rb")
-  readChar(soundfile, nchars = 8)        ## ChunkId and ChunkSize (4,4)
-  if(readChar(soundfile, nchars = 4) != 'WAVE'){
-    close(soundfile)
-    stop ("File provided is not in .wav format.")
-  }
-
-  readBin(soundfile, "integer", n = 10, size = 1)  ## Subchunk1ID, Subchunk1Size, AudioFormat (4,4,2)
-  
-  numChannels = readBin(soundfile, "integer", n = 1, size = 2)
-  if (numChannels > 1){
-   stop ("This function only loads mono (single-channel) WAV files.")
-  }
-  sampleRate = readBin(soundfile,"integer", n = 1, size = 4)
-
-  readBin(soundfile,"integer",n= 6,size=1)   ## ByteRate, BlockAlign (4,2)         
-
-  bitsPerSample = readBin(soundfile, "integer", size = 2)
-  if (bitsPerSample > 16) stop ("This function only loads 8 and 16 bit WAV files.")
-
-  readBin(soundfile,"integer",n= 4,size=1)  ## Subchunk2ID
-  subchunk2Size = readBin(soundfile,"integer", size=4)
-
-  if (bitsPerSample == 8)
-      sound = (readBin (soundfile, "integer" , n = subchunk2Size, size = 1, signed = FALSE) - 128) / 255
-  if (bitsPerSample == 16)
-      sound = readBin(soundfile,"integer", n = subchunk2Size/2, size=2, signed = TRUE) / 32768
  
-  close(soundfile)
+loadsound = function (filename=NA, channel = 'left'){
+  if (is.na(filename)) filename = file.choose()
 
-  numSamples = subchunk2Size / (bitsPerSample/8)
-  fs = sampleRate
+  snd = tuneR::readWave (filename)
+  
+  numSamples = length(snd@left)
+  fs = snd@samp.rate
+  
+  sound = snd@left
+  if (channel == 'right') sound = snd@right
 
   output = list (filename = filename, fs = fs, numSamples = numSamples,
-  duration = numSamples/fs * 1000, sound = ts(sound, frequency = fs, start=0))
+                 duration = numSamples/fs * 1000, sound = ts(sound, frequency = fs, start=0),
+                 bit = snd@bit)
   class(output) = "sound"
   output  
 }
