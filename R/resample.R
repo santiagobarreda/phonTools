041aac922401b0231d391bf5013345a3ec46ef54
Santiago Barreda <sbarreda@ucdavis.edu>
2024-02-28 09:47:55 -0800

    filter update

--- R/resample.R
+++ R/resample.R
@@ -1,8 +1,7 @@
 
 #' Resample a Sound
 #' 
-#' Resample using sinc interpolation.
-#' 
+#' Resample using sinc interpolation.#' 
 #' 
 #' @export
 #' @param sound Either a numeric vector representing a sequence of samples
@@ -13,27 +12,33 @@
 #' provided, this does not need to be specified.
 #' @param precision The number of samples before and after the current point to
 #' be used for interpolation.
-#' @param filterorder The number of taps to be used for the low-pass FIR
+#' @param filterorder The number of taps to be used for the low-pass Butterworth
 #' filters used, where appropriate.
-#' @param synthfilter If TRUE, synthfilter() is used for filtering.
+#' @param n_passes The number of passes through the low pass filter.
 #' @return The resampled vector is returned. If a 'sound' object is passed, the
 #' resampled sound is returned as an object.
 #' @author Santiago Barreda <sbarreda@@ucdavis.edu>
 #' @examples
-#' 
+#' \dontrun{
 #' data (sound)
-#' ## downsample and then upsample the sound back to 
-#' ## its original sampling frequency
-#' #downsamped = resample (sound, 11025)
-#' #upsamped = resample (downsamped, 22050)
+#' # downsample and then upsample the sound back to 
+#' # its original sampling frequency
+#' downsamped = resample (sound, 11025)
+#' upsamped = resample (downsamped, 22050)
 #' 
 #' # compare a part of the waveforms for all three sounds
-#' #par (mfrow = c(3,1), mar = c(4,4,1,1))
-#' #plot (sound$sound[1:14000], type = 'l')
-#' #plot (upsamped$sound[1:14000], type = 'l', col = 2)
-#' #plot (downsamped$sound[1:7000], type = 'l', col = 4)
+#' par (mfcol = c(3,2), mar = c(4,4,1,1))
+#' plot (sound$sound[1:14000], type = 'l')
+#' plot (downsamped$sound[1:7000], type = 'l', col = 4)
+#' plot (upsamped$sound[1:14000], type = 'l', col = 2)
 #' 
-resample = function (sound, newfs, oldfs, precision = 50, filterorder = 200, synthfilter = FALSE){
+#' phonTools::spectralslice(sound, xlim = c(0,11025),ylim=c(-90,5))
+#' phonTools::spectralslice(downsamped, xlim = c(0,11025),ylim=c(-90,5),col=4)
+#' phonTools::spectralslice(upsamped, xlim = c(0,11025),ylim=c(-90,5),col=2)
+#' }
+
+ 
+resample = function (sound, newfs, oldfs, precision = 50, filterorder = 6, n_passes = 4){
   soundout = 0; tsout = 0;
   if (inherits(sound,"ts")){
     fs = frequency(sound)
@@ -45,11 +50,11 @@ resample = function (sound, newfs, oldfs, precision = 50, filterorder = 200, syn
     oldfs = sound$fs
     sound = sound$sound
   } 
-  ratio = oldfs / newfs 
-  if (ratio > 1 & !synthfilter) sound = FIRfilter (sound, to = newfs/2, fs = oldfs, order = filterorder)
-  if (ratio > 1 & synthfilter) sound = synthfilter (sound, band = c(0,newfs/2), fs = oldfs)
+  ratio = newfs / oldfs
+  if (ratio < 1) 
+    sound = lowpass (sound, cutoff = ratio, order=filterorder, n_passes = n_passes)
   
-  newtime = seq (1, length(sound)+1, by = ratio)   
+  newtime = seq (1, length(sound)+1, by = 1/ratio)   
   nearest = round (newtime)                              
   offset = newtime - nearest                                 
   
@@ -59,8 +64,8 @@ resample = function (sound, newfs, oldfs, precision = 50, filterorder = 200, syn
   for (i in -precision:precision)
     y = y + sound[nearest+precision+i] * sinc(offset - i, normalized = TRUE)
   
-  if (ratio < 1 & !synthfilter) y = FIRfilter (y, to = oldfs/2, fs = newfs, order = filterorder)
-  if (ratio < 1 & synthfilter) y = synthfilter (y, band = c(0,oldfs/2), fs = newfs)
+  if (ratio > 1) 
+    y = lowpass (y, cutoff = oldfs/newfs, order= filterorder, n_passes = n_passes)
   
   sound = y / (max(y) * 1.05) 
   if (soundout == 1)  sound = makesound (sound, filename = oldsound$filename, fs = newfs)
