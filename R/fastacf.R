6acc4a174ecf078d2e1fb58dc5790b9877d01fb2
Santiago Barreda <sbarreda@ucdavis.edu>
2026-02-12 07:56:41 -0800

    Update fastacf.R

--- R/fastacf.R
+++ R/fastacf.R
@@ -48,19 +48,19 @@ fastacf = function (signal, lag.max = length(signal), window = 'hann',
   if (lag.max > n) lag.max = n 
   
   signal = signal - mean (signal)
-  signal = c(signal*windowfunc(signal, type = window), zeros (signal))
+  signal = c(signal*windowfunc(signal, type = window), rep(0, length(signal)))
   
   pad=0
   #if (pad){  
   #  pad = 2^ceiling (log (length (signal),2)) - length (signal)
-  #  signal = c(signal, zeros (pad))
+  #  signal = c(signal, rep(0, pad))
   #}
   s = fft (signal)
   s = Re (fft (s * Conj(s), inverse = TRUE))
   s = s[1:(lag.max)] / s[1]
   
   if (correct){
-    w = fft (c(windowfunc (n, type = window), zeros(n), zeros(pad)))
+    w = fft (c(windowfunc (n, type = window), rep(0, n), rep(0, pad)))
     w = Re (fft (w * Conj(w), inverse = TRUE))
     w = w[1:(lag.max)] / w[1]
     s = s/w
