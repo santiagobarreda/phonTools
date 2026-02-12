a129a18313e80a502b0d26896002422847946bca
Santiago Barreda <sbarreda@ucdavis.edu>
2026-02-12 07:57:22 -0800

    Update print.spectrogram.R

--- R/print.spectrogram.R
+++ R/print.spectrogram.R
@@ -1,4 +1,22 @@
 
+#' Print Spectrogram Objects
+#'
+#' Display information about a spectrogram object.
+#'
+#' Prints a brief message about a spectrogram object and indicates that it
+#' should be plotted using plot().
+#'
+#' @method print spectrogram
+#' @export
+#' @param x A spectrogram object to be printed.
+#' @param ... Unused, for compatibility with generic print method.
+#' @return Invisibly returns x. Used for side effects (printing).
+#' @author Santiago Barreda <sbarreda@@ucdavis.edu>
+#' @examples
+#' data(sound)
+#' sp <- spectrogram(sound)
+#' print(sp)
+#'
 #' @export
 
 print.spectrogram <-
