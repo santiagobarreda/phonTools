c4b86be49dab0343d6f272fdb76fa76b538fc111
Santiago Barreda <sbarreda@ucdavis.edu>
2026-02-12 07:57:19 -0800

    Update print.sound.R

--- R/print.sound.R
+++ R/print.sound.R
@@ -1,4 +1,21 @@
 
+#' Print Sound Objects
+#'
+#' Display information about a sound object.
+#'
+#' Prints summary information about a sound object including the filename,
+#' sampling frequency, duration, and number of samples.
+#'
+#' @method print sound
+#' @export
+#' @param x A sound object to be printed.
+#' @param ... Unused, for compatibility with generic print method.
+#' @return Invisibly returns x. Used for side effects (printing).
+#' @author Santiago Barreda <sbarreda@@ucdavis.edu>
+#' @examples
+#' data(sound)
+#' print(sound)
+#'
 #' @export
 
 print.sound = function (x, ...){
