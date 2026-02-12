4fc41c0fbd8e1572dd63c32b37b0b81a78182f51
Santiago Barreda <sbarreda@ucdavis.edu>
2026-02-12 07:57:14 -0800

    Update plot.sound.R

--- R/plot.sound.R
+++ R/plot.sound.R
@@ -1,4 +1,22 @@
 
+#' Plot Sound Objects
+#'
+#' Plot the waveform of a sound object.
+#'
+#' Creates a time-domain plot of a sound object's waveform. If the sound is
+#' stored as a time series (ts) object, R's built-in plotting is used.
+#' Otherwise, time is computed from the sampling frequency.
+#'
+#' @method plot sound
+#' @export
+#' @param x A sound object to be plotted.
+#' @param ... Additional graphical parameters passed to the plot function.
+#' @return Invisibly returns NULL. Used for side effects (plotting).
+#' @author Santiago Barreda <sbarreda@@ucdavis.edu>
+#' @examples
+#' data(sound)
+#' plot(sound)
+#'
 #' @export
 
 plot.sound = function (x, ...){
